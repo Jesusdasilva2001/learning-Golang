fb0c82093d8a025e13b72c389b7c6e70a44ce385
Jesus da silva Tamba <jesustamba2017@gmail.com>
2023-07-19 17:44:51 +0100

    second commit

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/Jesusdasilva2001/learning-Golang/internal/entity/infra/database"
+	"github.com/Jesusdasilva2001/learning-Golang/internal/entity/usecase"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+
 func main() {
-	println("Hello, world!")
+	db, err := sql.Open("sqlite3", "db.sqlite3")
+	if err != nil {
+		panic(err)
+	}
+
+	orderRepository := database.NewOrderRepository(db)
+	uc := usecase.NewCalculateFinalPrice(orderRepository)
+
+	input := usecase.OrderInput{
+		ID:    "1234",
+		Price: 10.0,
+		Tax:   1.0,
+	}
+	output, err := uc.Execute(input)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(output)
 }
